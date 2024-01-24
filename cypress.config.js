cfc7ee95077a2f83603e74aa659679beea02bcea
LiujieHe <268079@163.com>
2024-01-24 15:11:39 +0800

    update

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,6 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   projectId: "pwh6zn",
-  viewportHeight: 600,
-  viewportWidth: 300,
   e2e: {
     baseUrl: 'http://localhost:4500',
   },
