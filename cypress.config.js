1703052542a4fc0e4b58ae1b3204da69afa55014
LiujieHe <268079@163.com>
2024-01-24 16:13:58 +0800

    change

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,10 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   projectId: "pwh6zn",
+  env: {
+    username: 'test',
+    password: 'test',
+  },
   e2e: {
     baseUrl: 'http://localhost:4500',
   },
