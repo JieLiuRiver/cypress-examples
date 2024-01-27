18b954c8bc92f7993c067a8200a3ccccceb9a4c9
LiujieHe <268079@163.com>
2024-01-27 09:55:53 +0000

    change code

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,47 +1,26 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import FilterLink from '../containers/FilterLink'
-import { SHOW_ALL, SHOW_COMPLETED, SHOW_ACTIVE } from '../constants/TodoFilters'
+import PropTypes from 'prop-types';
+import React from 'react';
+import TodoTextInput from './TodoTextInput';
 
-const FILTER_TITLES = {
-  [SHOW_ALL]: 'All',
-  [SHOW_ACTIVE]: 'Active',
-  [SHOW_COMPLETED]: 'Completed'
+const Header = ({ addTodo }) => (
+  <header className="header">
+    <h1>todos</h1>
+    <TodoTextInput
+      newTodo
+      onSave={(text) => {
+        if (text.length !== 0) {
+          // simulate delayed application logic
+          // setTimeout(addTodo, 1000, text)
+          addTodo(text)
+        }
+      }}
+      placeholder="What needs to be done?"
+    />
+  </header>
+)
+
+Header.propTypes = {
+  addTodo: PropTypes.func.isRequired
 }
 
-const Footer = (props) => {
-  const { activeCount, completedCount, onClearCompleted } = props
-  const itemWord = activeCount === 1 ? 'item' : 'items'
-  return (
-    <footer className="footer">
-      <span className="todo-count">
-        <strong>{activeCount || 'No'}</strong> {itemWord} left
-      </span>
-      <ul className="filters">
-        {Object.keys(FILTER_TITLES).map(filter =>
-          <li key={filter}>
-            <FilterLink filter={filter}>
-              {FILTER_TITLES[filter]}
-            </FilterLink>
-          </li>
-        )}
-      </ul>
-      {
-        !!completedCount &&
-        <button
-          className="clear-completed"
-          onClick={onClearCompleted}
-        >Clear completed</button>
-        
-      }
-    </footer>
-  )
-}
-
-Footer.propTypes = {
-  completedCount: PropTypes.number.isRequired,
-  activeCount: PropTypes.number.isRequired,
-  onClearCompleted: PropTypes.func.isRequired,
-}
-
-export default Footer
+export default Header
