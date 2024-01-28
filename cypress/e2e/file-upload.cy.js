c0bc762b7cad4864f062b614015eed3923dc59ec
LiujieHe <268079@163.com>
2024-01-28 13:37:06 +0800

    test: for file upload

--- cypress/e2e/file-upload.cy.js
+++ cypress/e2e/file-upload.cy.js
@@ -3,9 +3,28 @@
 describe('File Uploads', function () {
     it('Single file upload', () => {
         cy.visit("http://the-internet.herokuapp.com/upload")
-        cy.get('#file-upload').attachFile('art.png')
+        cy.get('#file-upload').attachFile('cypress-logo.png')
         cy.get('#file-submit').click()
         cy.wait(5000)
         cy.get('div[class="example"] h3').should('have.text', 'File Uploaded!');
     })
+
+    it('File upload - Rename', () => {
+        cy.visit("http://the-internet.herokuapp.com/upload")
+        cy.get('#file-upload').attachFile({
+            filePath: 'cypress-logo.png',
+            fileName: 'james'
+        })
+        cy.get('#file-submit').click()
+        cy.wait(5000)
+        cy.get('div[class="example"] h3').should('have.text', 'File Uploaded!');
+    })
+
+    it('File upload - drag & drop', () => {
+        cy.visit("http://the-internet.herokuapp.com/upload")
+        cy.get('#drag-drop-upload').attachFile('cypress-logo.png', {subjectType: 'drag-n-drop'})
+        cy.wait(5000)
+        cy.get('#drag-drop-upload > .dz-preview > .dz-details > .dz-filename > span').contains('cypress-logo.png');
+    })
+
 })
